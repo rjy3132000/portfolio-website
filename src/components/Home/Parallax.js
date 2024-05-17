93ccb1ec9fbe58479f70735a06702b2d8d8c96f3
Rajesh <ry292000@gmail.com>
2024-05-17 23:33:22 +0530

    completed the parallax scroll motion

--- src/components/Home/Parallax.js
+++ src/components/Home/Parallax.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import "../../assests/css/parallax.scss";
 
 const Parallax = () => {
-  return <section id="Portfolio">Parallax</section>;
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
+  const yBG = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <section id="Portfolio" className="Parallax">
+      <div className="wrapper" ref={ref}>
+        <motion.h3 style={{ y: yText }}>what we do?</motion.h3>
+        <motion.div className="mountains"></motion.div>
+        <motion.div style={{ y: yBG }} className="planets"></motion.div>
+        <motion.div style={{ x: yBG }} className="stars"></motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Parallax;
--- src/components/Home/Parallax1.js
+++ src/components/Home/Parallax1.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import "../../assests/css/parallax.scss";
 
 const Parallax1 = () => {
-  return <section>Parallax1</section>;
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
+  const yBG = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <section className="Parallax1">
+      <div className="wrapper" ref={ref}>
+        <motion.h3 style={{ y: yText }}>what we did</motion.h3>
+        <motion.div className="mountains"></motion.div>
+        <motion.div style={{ y: yBG }} className="planets"></motion.div>
+        <motion.div style={{ x: yBG }} className="stars"></motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Parallax1;
